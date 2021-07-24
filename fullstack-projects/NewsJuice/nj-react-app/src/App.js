c235063ffc0830a9ea0359832cd3eee5d2759bd8
Amaresh <amaresh.satapathy@gmail.com>
2021-07-24 10:02:21 +0530

    Adding Tab component

--- fullstack-projects/NewsJuice/nj-react-app/src/App.js
+++ fullstack-projects/NewsJuice/nj-react-app/src/App.js
@@ -4,11 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  import {Tabs, Tab, Modal, Row, Button, Col, Form, Card, Container} from "react-bootstrap";
  import Home from "./components/Home/Home";
  import Profile from "./components/Profile/Profile";
-import Analysis from "./"
-import Demo from "./demo"
+import Analysis from "./";
+import MyTab from "./MyTab"
 function App() {
   return (
- <Demo />   
+ <MyTab />   
   );
 }
 
