c0d9c0d1f3c343c3e78872e22b94818a2bec253c
Amaresh <amaresh.satapathy@gmail.com>
2021-07-24 09:49:14 +0530

    Added Tab components and form template

--- fullstack-projects/NewsJuice/nj-react-app/src/App.js
+++ fullstack-projects/NewsJuice/nj-react-app/src/App.js
@@ -4,30 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  import {Tabs, Tab, Modal, Row, Button, Col, Form, Card, Container} from "react-bootstrap";
  import Home from "./components/Home/Home";
  import Profile from "./components/Profile/Profile";
- 
+import Analysis from "./"
+import Demo from "./demo"
 function App() {
   return (
- <Container>
-                 <Row>
-                     <Col>
-                         <Tabs defaultActiveKey="Home" 
-                               id="controlled-tab-example">
-                             <Tab eventKey="home" title="Home">
-                             <Home />
-                             </Tab>
-                             <Tab eventKey="profile" title="Profile">
-                                 <Profile />
-                             </Tab>
-                             <Tab eventKey="contact" title="Contact" disabled>
-                              
-                             </Tab>
-                         </Tabs>
-                     </Col>
-                 </Row>
-                 <Row>
-                   Hello
-                 </Row>
-             </Container>
+ <Demo />   
   );
 }
 
--- fullstack-projects/NewsJuice/nj-react-app/src/components/Profile/Profile.js
+++ fullstack-projects/NewsJuice/nj-react-app/src/components/Profile/Profile.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Profile.module.css';
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },  button: {
+    margin: theme.spacing(1)
+  },
+}));
 
-const Profile = () => (
-  <div className={styles.Profile}>
-    Profile Component
-  </div>
-);
 
-Profile.propTypes = {};
+function Profile(props){
+  const classes = useStyles();
+  const [name,setName] =  useState("");
 
-Profile.defaultProps = {};
+  const handleSubmit = (evt) =>{
+    evt.preventDefault();
+    alert(`Submitting Name ${name}`);
+  }
 
+  return (
+    <Box display="flex">
+<form className={classes.root} noValidate autoComplete="off">
+  <TextField id="standard-basic" label="Name" onChange={e => setName(e.target.value)} />
+  <TextField id="standard-basic" label="Surname"/>
+  <TextField id="standard-basic" label="Age" />
+  <Box display="flex" flexDirection="column" >
+   <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        className={classes.button}
+        startIcon={<SaveIcon />}  onClick={handleSubmit}
+      >
+        Save
+      </Button>
+</Box>
+ 
+</form>
+</Box>
+  );
+
+
+}
 export default Profile;
