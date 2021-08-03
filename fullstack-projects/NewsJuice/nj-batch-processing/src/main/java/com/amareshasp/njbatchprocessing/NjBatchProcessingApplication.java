6a7fe664484e420f34af7e98840c52f011b7416d
Amaresh <amaresh.satapathy@gmail.com>
2021-08-03 12:05:57 +0530

    NewsJuice batch processing application initial commit

---
+++ fullstack-projects/NewsJuice/nj-batch-processing/src/main/java/com/amareshasp/njbatchprocessing/NjBatchProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.amareshasp.njbatchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjBatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjBatchProcessingApplication.class, args);
+	}
+
+}
---
+++ fullstack-projects/NewsJuice/nj-batch-processing/src/test/java/com/amareshasp/njbatchprocessing/NjBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amareshasp.njbatchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
