226877c645ba47496fb312041d620b2451e0d90d
Amaresh <amaresh.satapathy@gmail.com>
2021-08-04 17:27:28 +0530

    UI Form layout fixes

--- fullstack-projects/NewsJuice/nj-react-app/src/components/Profile/Profile.js
+++ fullstack-projects/NewsJuice/nj-react-app/src/components/Profile/Profile.js
@@ -1,53 +1,102 @@
-import React, {useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Icon from '@material-ui/core/Icon';
-import SaveIcon from '@material-ui/icons/Save';
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },  button: {
-    margin: theme.spacing(1)
-  },
-}));
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import styles from "./Profile.module.css";
+import TextField from "@material-ui/core/TextField";
+import Icon from "@material-ui/core/Icon";
+import SaveIcon from "@material-ui/icons/Save";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
+function Profile(props) {
+  const [name, setName] = useState("");
 
-function Profile(props){
-  const classes = useStyles();
-  const [name,setName] =  useState("");
-
-  const handleSubmit = (evt) =>{
+  const handleSubmit = (evt) => {
     evt.preventDefault();
     alert(`Submitting Name ${name}`);
-  }
+  };
 
   return (
     <Box display="flex">
-<form className={classes.root} noValidate autoComplete="off">
-  <TextField id="standard-basic" label="Name" onChange={e => setName(e.target.value)} />
-  <TextField id="standard-basic" label="Surname"/>
-  <TextField id="standard-basic" label="Age" />
-  <Box display="flex" flexDirection="column" >
-   <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        className={classes.button}
-        startIcon={<SaveIcon />}  onClick={handleSubmit}
-      >
-        Save
-      </Button>
-</Box>
- 
-</form>
-</Box>
+      <form className={styles.accord} noValidate autoComplete="off">
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>USER</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <TextField
+                id="standard-basic"
+                label="Name"
+                className={styles.formfield}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                id="standard-basic"
+                label="Surname"
+                className={styles.formfield}
+              />
+              <TextField
+                id="standard-basic"
+                label="Age"
+                className={styles.formfield}
+              />
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>SUBSCRIPTION</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <TextField
+                id="standard-basic"
+                label="Agency"
+                className={styles.formfield}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                id="standard-basic"
+                label="Channel"
+                className={styles.formfield}
+              />
+              <TextField
+                id="standard-basic"
+                label="Filter"
+                className={styles.formfield}
+              />
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Box display="flex" flexDirection="row-reverse">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            className={styles.button}
+            startIcon={<SaveIcon />}
+            onClick={handleSubmit}
+          >
+            Save
+          </Button>
+        </Box>
+      </form>
+    </Box>
   );
-
-
 }
 export default Profile;
