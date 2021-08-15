bb3ed54bafa20cfd7486515c2d7f8d356e40b28c
Amaresh <amaresh.satapathy@gmail.com>
2021-08-15 17:34:33 +0530

    user profile form dynamic dropdown option fixed.

--- fullstack-projects/NewsJuice/nj-ng-app/src/app/components/user-form/user-form.component.ts
+++ fullstack-projects/NewsJuice/nj-ng-app/src/app/components/user-form/user-form.component.ts
@@ -1,88 +1,106 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder,FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.css']
+  styleUrls: ['./user-form.component.css'],
 })
 export class UserFormComponent implements OnInit {
-
-  myForm: FormGroup = new FormGroup({}) ;
-
-  constructor(private fb : FormBuilder,   private http: HttpClient) { 
-  
-
-  }
+  myForm: FormGroup = new FormGroup({});
+  newsAgencyList: string[] = [];
+  newsAgency: any[] = [
+    { name: 'The Times of India' },
+    { name: 'The Hindu' },
+    { name: 'Deccan Herald' },
+    { name: 'Hindustan Times ' },
+    { name: 'The Economics Times' },
+    { name: 'The Indian Express' },
+    { name: 'The Telegraph' },
+  ];
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
-      this.myForm = this.fb.group({
+    this.myForm = this.fb.group({
       name: '',
-      age:'',
+      age: '',
       email: '',
       subs: this.fb.array([
-          this.fb.group({
-         agency: [''] ,
-         sport: [''],
-         event: [''],
-         entertainment: [''],
-         politics: [''],
-         tech: [''],
-       })
-      ])
+        this.fb.group({
+          agency: [''],
+          sport: [''],
+          event: [''],
+          entertainment: [''],
+          politics: [''],
+          tech: [''],
+        }),
+      ]),
     });
 
-      this.http.get('/assets/formdata.json')
-      .subscribe((data: any) => {
-        this.myForm = this.fb.group({
-          name: new FormControl(data.name),
-          age: data.age,
-          email: data.email,
-          subs: this.fb.array(data.subs.map((datum: any)=> this.generateDatumFormGroup(datum)))
-        });
+    this.http.get('/assets/formdata.json').subscribe((data: any) => {
+      this.myForm = this.fb.group({
+        name: new FormControl(data.name),
+        age: data.age,
+        email: data.email,
+        subs: this.fb.array(
+          data.subs.map((datum: any) => this.generateDatumFormGroup(datum))
+        ),
       });
+    });
 
-
-/*this.addPhoneTOISports();*/
-  this.myForm.valueChanges.subscribe(console.log);
-
+    /*this.addPhoneTOISports();*/
+    this.myForm.valueChanges.subscribe(console.log);
   }
 
-  
-  private generateDatumFormGroup(datum:any) {
+  private generateDatumFormGroup(datum: any) {
     console.log(datum);
-    return this.fb.group( {
-      agency:  new FormControl(datum.agency),
+      //this.newsAgency = this.newsAgency.filter(item => item === datum.agency);  
+    this.newsAgencyList.push(datum.agency);
+    const obj = this.fb.group({
+      agency: new FormControl(datum.agency),
       sport: new FormControl(datum.sport),
       event: new FormControl(datum.event),
       entertainment: new FormControl(datum.entertainment),
       politics: new FormControl(datum.politics),
-      tech: new FormControl(datum.tech)                        
+      tech: new FormControl(datum.tech),
     });
+
+return obj;
   }
 
-  get newSubs(){
-return this.myForm.get('subs') as FormArray;
+  get newSubs() {
+    return this.myForm.get('subs') as FormArray;
   }
 
-  
-  addPhoneTOISports(){
+  addPhoneTOISports() {
     const subs = this.fb.group({
-      agency:'TOI',
-      sport:true
-    })
+      agency: 'TOI',
+      sport: true,
+    });
     this.newSubs.push(subs);
   }
-  addPhone(){
+  addPhone() {
     const subs = this.fb.group({
-      agency:[],
-      sport:[]
-    })
+      agency: [''],
+      sport: [''],
+      event: [''],
+      entertainment: [''],
+      politics: [''],
+      tech: [''],
+    });
     this.newSubs.push(subs);
   }
-  deletePhone(i: number){
+  deletePhone(i: number) {
     this.newSubs.removeAt(i);
+  }
+
+  isDisabled(value : string) {
+    const i = this.newsAgencyList.indexOf(value);
+    if (i >= 0){
+      return true;
+    }
+    return false;
   }
 
 }
